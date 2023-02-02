80e0cb4660b1c340270639f54500ada1155b695f
Luiz Campedelli <vapeprosper@gmail.com>
2023-02-01 22:10:44 -0300

    pages_controller_methods

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_31_235930) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_01_012005) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "pool_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pool_id"], name: "index_orders_on_pool_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "pools", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +40,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_31_235930) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "orders", "pools"
+  add_foreign_key "orders", "users"
 end
