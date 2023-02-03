cb5d57bfcd6ed45f6cf16ded133b1353ab3df625
Taisa Silva <taisa@travelmedia.com.br>
2023-02-03 05:50:17 -0300

    edit-new-funcionando

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -31,3 +31,9 @@ class PoolsController < ApplicationController
     redirect_to pool_path(@pool)
   end
 end
+
+private
+
+  def pool_params
+    params.require(:pool).permit(:name, :size, :price, :user_id)
+  end
