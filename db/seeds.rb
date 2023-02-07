01451c7eb55aac6af3f3240576b7bce9bd9dbfb4
Marina Lameirao <marinalameirao@hotmail.com>
2023-02-07 19:14:40 -0300

    search

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,19 +17,19 @@ User_Maria = User.create!(email: "maria@terra.com.br", password: "1234fgh")
 puts"Creating Pools"
 
 
-Julio_Delamare = Pool.new(user: User_Maria, name: "Parque Aquático Julio Delamare", size: 100, price: 200_000, address: "Av. Maracanã, 12 - Maracanã, Rio de Janeiro")
+Julio_Delamare = Pool.new(user: User_Maria, name: "Parque Aquático Julio Delamare", size: 100, price: 200_000, address: "Av. Maracanã, 12 - Maracanã, Rio de Janeiro", capacity: 10)
 Julio_Delamare.save!
 
-Ofuro_Vidigal = Pool.new(user: User_Maria, name: "Ofurô no Vigigal", size: 2, price: 20_000, address: "R. Armando Almeida Lima, 8 - Vidigal, Rio de Janeiro")
+Ofuro_Vidigal = Pool.new(user: User_Maria, name: "Ofurô no Vigigal", size: 2, price: 20_000, address: "R. Armando Almeida Lima, 8 - Vidigal, Rio de Janeiro", capacity: 20)
 Ofuro_Vidigal.save!
 
-Piscina_Luxo_Jardins = Pool.new(user: User_Maria, name: "Piscina Luxo no Jardins", size: 50, price: 150_000, address: "Rua Vittorio Fasano, 88, São Paulo")
+Piscina_Luxo_Jardins = Pool.new(user: User_Maria, name: "Piscina Luxo no Jardins", size: 50, price: 150_000, address: "Rua Vittorio Fasano, 88, São Paulo", capacity: 5)
 Piscina_Luxo_Jardins.save!
 
-Piscina_bowl_skate = Pool.new(user: User_Maria, name: "Piscina bowl para skate", size: 50, price: 30_000, address: "Av. Queiroz Filho, 1365 Vila Hamburguesa, São Paulo")
+Piscina_bowl_skate = Pool.new(user: User_Maria, name: "Piscina bowl para skate", size: 50, price: 30_000, address: "Av. Queiroz Filho, 1365 Vila Hamburguesa, São Paulo", capacity: 5)
 Piscina_bowl_skate.save!
 
-Piscina_borda_infinita = Pool.new(user: User_Maria, name: "Piscina borda infinita Ilha Bela", size: 20, price: 50_000, address: "Rua Jackson de Figueiredo 501, Joatinga, Rio de Janeiro")
+Piscina_borda_infinita = Pool.new(user: User_Maria, name: "Piscina borda infinita Ilha Bela", size: 20, price: 50_000, address: "Rua Jackson de Figueiredo 501, Joatinga, Rio de Janeiro", capacity: 20)
 Piscina_borda_infinita.save!
 
 puts "Finished!"
