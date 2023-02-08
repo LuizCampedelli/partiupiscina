84113c67d5b03bc6dff2acc3271588e31e804e4d
savanne soares <savannesoares88@gmail.com>
2023-02-07 22:16:20 -0300

    ajustadas as fotos do show

--- app/models/pool.rb
+++ app/models/pool.rb
@@ -3,5 +3,6 @@ class Pool < ApplicationRecord
   validates :name, :price, :size, presence: true
   validates :price, numericality: { only_integer: true }
   has_many_attached :photos
+  #validates :photos, presence: true
   #validates :schedule, inclusion: [true, false]
 end
