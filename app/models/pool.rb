3bb32b61362e1e3abddebb13e93e7c445c8f2bcd
Marina Lameirao <marinalameirao@hotmail.com>
2023-02-09 12:47:31 -0300

    button and pool models

--- app/models/pool.rb
+++ app/models/pool.rb
@@ -1,5 +1,6 @@
 class Pool < ApplicationRecord
   belongs_to :user
+  has_many :orders
   validates :name, :price, :size, :capacity, presence: true
   validates :price, :capacity, numericality: { only_integer: true }
   has_many_attached :photos
